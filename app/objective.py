b5341250155b8388640fb59da0a1b9e59b7caf03
Max Verbeek <m4xv3rb33k@gmail.com>
2017-03-04 19:22:11 +0100

    Render text dat bij een objective hoort

--- app/objective.py
+++ app/objective.py
@@ -1,7 +1,9 @@
 import pygame
-from constants import BLOCKSIZE
+import constants
+from constants import BLOCKSIZE, FONT_NAME
+from player import Entity
 
-class Objective(pygame.sprite.Sprite):
+class Objective(Entity):
 	
 	def __init__(self, x, y):
 		super().__init__()
@@ -9,8 +11,29 @@ class Objective(pygame.sprite.Sprite):
 
 		self.rect = pygame.Rect(x, y, BLOCKSIZE, BLOCKSIZE)
 
+		self.text = pygame.font.SysFont(FONT_NAME, 16).render("<html>", 1, constants.RED)
+
 
 
 
 class ObjectiveList(pygame.sprite.Group):
-	pass
+
+	def draw(self, surface):
+
+		"""draw all sprites onto the surface
+		Group.draw(surface): return None
+		Draws all of the member sprites onto the given surface.
+		"""
+
+		# Lekker van de source gejat: https://bitbucket.org/pygame/pygame/src/407caa445ee033bfbcf5e9cc5c3961c0aa9db8f4/lib/sprite.py?at=default&fileviewer=file-view-default#sprite.py-464
+		sprites = self.sprites()
+		surface_blit = surface.blit
+		for spr in sprites:
+			self.spritedict[spr] = surface_blit(spr.image, spr.rect)
+
+			# render de tekst boven de objectives
+			x = spr.rect.centerx - spr.text.get_width() / 2 # in het midden van het objective
+			y = spr.rect.y - spr.text.get_height() - 5 # 5 px boven objective
+			surface_blit(spr.text, (x, y))
+
+		self.lostsprites = []
