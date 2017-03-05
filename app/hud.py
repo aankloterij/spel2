f44fc91668da41c873fb97241cba469ce958dab2
Sibren Talens <sibrentalens@gmail.com>
2017-03-05 18:39:51 +0100

    Change font and change player

--- app/hud.py
+++ app/hud.py
@@ -36,42 +36,42 @@ class GameMenu():
 		self.screen = screen
 		self.scr_width = self.screen.get_rect().width
 		self.scr_height = self.screen.get_rect().height
- 
+
 		self.bg_color = bg_color
 		self.clock = pygame.time.Clock()
- 
+
 		self.items = items
-		self.font = pygame.font.SysFont(constants.FONT_NAME, font_size)
+		self.font = pygame.font.Font('res/Pixeled.ttf', font_size)
 		self.font_color = font_color
- 
+
 		self.items = []
 		for index, item in enumerate(items):
 			label = self.font.render(item, 1, font_color)
- 
+
 			width = label.get_rect().width
 			height = label.get_rect().height
- 
+
 			posx = (self.scr_width / 2) - (width / 2)
 			# t_h: total height of text block
 			t_h = len(items) * height
 			posy = (self.scr_height / 2) - (t_h / 2) + (index * height)
- 
+
 			self.items.append([item, label, (width, height), (posx, posy)])
- 
+
 	def run(self):
 		mainloop = True
 		while mainloop:
 			# Limit frame speed to 50 FPS
 			self.clock.tick(50)
- 
+
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT:
 					mainloop = False
- 
+
 			# Redraw the background
 			self.screen.fill(self.bg_color)
- 
+
 			for name, label, (width, height), (posx, posy) in self.items:
 				self.screen.blit(label, (posx, posy))
- 
+
 			pygame.display.flip()
